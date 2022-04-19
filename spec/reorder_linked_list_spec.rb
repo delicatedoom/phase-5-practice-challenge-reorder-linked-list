2fc9bb3b99b9dfc6b9034b838f3d4d44212b2187
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-04-19 14:09:00 -0600

    commit tree d2e894f33ab22bd005b01b7f9c7e186d5d2ff6c7

--- spec/reorder_linked_list_spec.rb
+++ spec/reorder_linked_list_spec.rb
@@ -1,86 +1,29 @@
-RSpec.describe 'reorder_linked_list' do
-  it 'Handles cases when the linked list is empty' do
-    linked_list = LinkedList.new
-    linked_list.reorder_linked_list
-    expect(linked_list.head).to eq(nil)
+def reorder_linked_list
+  # if the list is empty, no need to continue
+  return if head.nil?
+
+  # set odd to the first odd node and even to the first even node
+  odd = head
+  even = head.next_node
+
+  # keep a reference to the first even node
+  even_head = even
+
+  while even && even.next_node
+    # change odd's next_node property to point to the next odd node
+    odd.next_node = even.next_node
+
+    # reset the odd variable to that node
+    odd = odd.next_node
+
+    # change even's next_node property to point to the node after
+    # the new odd node, i.e., the next even node
+    even.next_node = odd.next_node
+
+    # reset the even variable to that node
+    even = even.next_node
   end
 
-  it 'Handles cases when the linked list has one node' do
-    linked_list = LinkedList.new(Node.new(1))
-    linked_list.reorder_linked_list
-    expect(linked_list.head.data).to eq(1)
-  end
-
-  it 'Handles cases when the linked list has two nodes' do
-    # 1 -> 2
-    head = Node.new(1, Node.new(2))
-    linked_list = LinkedList.new(head)
-
-    # 1 -> 2
-    linked_list.reorder_linked_list
-
-    node1 = linked_list.head
-    expect(node1.data).to eq(1)
-
-    node2 = node1.next_node
-    expect(node2.data).to eq(2)
-  end
-
-  it 'Handles cases when the linked list has an even number of nodes' do
-    # 1 -> 2 -> 3 -> 4
-    head = Node.new(1, Node.new(2, Node.new(3, Node.new(4))))
-    linked_list = LinkedList.new(head)
-
-    # 1 -> 3 -> 2 -> 4
-    linked_list.reorder_linked_list
-
-    node1 = linked_list.head
-    expect(node1.data).to eq(1)
-
-    node2 = node1.next_node
-    expect(node2.data).to eq(3)
-
-    node3 = node2.next_node
-    expect(node3.data).to eq(2)
-
-    node4 = node3.next_node
-    expect(node4.data).to eq(4)
-  end
-
-  it 'Handles cases when the linked list has an odd number of nodes' do
-    # 2 -> 1 -> 3 -> 5 -> 6 -> 4 -> 7
-    head =
-      Node.new(
-        2,
-        Node.new(
-          1,
-          Node.new(3, Node.new(5, Node.new(6, Node.new(4, Node.new(7))))),
-        ),
-      )
-    linked_list = LinkedList.new(head)
-
-    # 2 -> 3 -> 6 -> 7 -> 1 -> 5 -> 4
-    linked_list.reorder_linked_list
-
-    node1 = linked_list.head
-    expect(node1.data).to eq(2)
-
-    node2 = node1.next_node
-    expect(node2.data).to eq(3)
-
-    node3 = node2.next_node
-    expect(node3.data).to eq(6)
-
-    node4 = node3.next_node
-    expect(node4.data).to eq(7)
-
-    node5 = node4.next_node
-    expect(node5.data).to eq(1)
-
-    node6 = node5.next_node
-    expect(node6.data).to eq(5)
-
-    node7 = node6.next_node
-    expect(node7.data).to eq(4)
-  end
+  # change the next_node property of the last odd node to point to the first even node
+  odd.next_node = even_head
 end
